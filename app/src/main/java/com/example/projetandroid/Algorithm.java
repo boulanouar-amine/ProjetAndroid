8c4b3ba0b0956dc68181820bd16653db70c2ed52
boulanouar <boulanouarmohamedamine@gmail.com>
2022-12-19 09:34:08 +0100

    first naive bayes implementation

--- app/src/main/java/com/example/projetandroid/Algorithm.java
+++ app/src/main/java/com/example/projetandroid/Algorithm.java
@@ -3,12 +3,14 @@ package com.example.projetandroid;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Algorithm {
 
-    // KNN section
+    // helper functions
 
     // Method to find minimum value from the arrayList
     static double minValue(ArrayList<Double> arraylist) {
@@ -45,7 +47,9 @@ public class Algorithm {
         }
             return normalizedList;
         }
-    public static TreeSet<Integer> NearestNeighbour(Personne targetPersonne, List<Personne> personnes) {
+
+        //KNN section
+    public static TreeSet<Integer> NearestNeighbour(Personne targetPersonne, List<Personne> personnes , int knumber){
 
   
         //age has to be normalized between 0 and 1 to be able to compare it with the other attributes
@@ -94,8 +98,8 @@ public class Algorithm {
 
             int cholesterolDifference = targetPersonne.getCholesterol().equals(p.getCholesterol()) ? 0 : 1;
 
-            double naDifference = Math.abs((double) targetPersonne.getNa() - (double) p.getNa());
-            double kDifference = Math.abs((double) targetPersonne.getK() - (double) p.getK());
+            double naDifference = Math.abs(targetPersonne.getNa() - p.getNa());
+            double kDifference = Math.abs(targetPersonne.getK() - p.getK()) * 10 ; // *10 to make it fit with the other attributes
 
             //total difference is the sum of all the differences between the target and the current personne in the loop
             //using a distance metric formula based on the euclidean distance (square root of the sum of the squares of the differences)
@@ -115,7 +119,7 @@ public class Algorithm {
         //TreeSet to store the 5 (can be changed to a variable later) nearest neighbours of the target
         TreeSet<Integer> nearestNeighbours = new TreeSet<>();
         
-        for(int Knumber = 1; Knumber <= 5; Knumber++) {
+        for(int i = 1; i <= knumber; i++) {
            nearestNeighbours.add(sortedDifferenceMap.get(sortedDifferenceMap.firstKey()));
             sortedDifferenceMap.remove(sortedDifferenceMap.firstKey());
         }
@@ -143,4 +147,167 @@ public class Algorithm {
     }
     
     // Start of Bayes section
+
+    public static void putInMap(Map<String, Integer> map, String key) {
+        if (map.containsKey(key)) {
+            map.put(key, map.get(key) + 1);
+        } else {
+            map.put(key, 1);
+        }
+    }
+
+    public static Double listeAverage(ArrayList<Double> liste) {
+        Double somme = 0.0;
+        for (Double valeur : liste) {
+            somme += valeur;
+        }
+        return somme / liste.size();
+    }
+
+    //method to initialize the arraylist of Personne Na
+    public static ArrayList<Double> initializeNa(List<Personne> personnes){
+        ArrayList<Double> naList = new ArrayList<>();
+        for (Personne personne : personnes) {
+            naList.add(personne.getNa());
+        }
+        return naList;
+    }
+
+    //method to initialize the arraylist of Personne K
+    //method to initialize the arraylist of Personne Na
+    public static ArrayList<Double> initializeK(List<Personne> personnes){
+        ArrayList<Double> kList = new ArrayList<>();
+        for (Personne personne : personnes) {
+            kList.add(personne.getK());
+        }
+        return kList;
+    }
+
+    public static String naiveBayes(Personne targetPersonne, List<Personne> personnes){
+
+        SortedMap<String,Integer> drugMap = new TreeMap<>();
+
+        //Age Map
+        SortedMap<String,Integer> AgeMapHigh = new TreeMap<>();
+        SortedMap<String,Integer> AgeMapLow = new TreeMap<>();
+        //Genre Map
+        SortedMap<String,Integer> drugGenreM = new TreeMap<>();
+        SortedMap<String,Integer> drugGenreF = new TreeMap<>();
+        //Bloodpressure Map
+        SortedMap<String,Integer> drugBloodPressureHigh = new TreeMap<>();
+        SortedMap<String,Integer> drugBloodPressureNormal = new TreeMap<>();
+        SortedMap<String,Integer> drugBloodPressureLow = new TreeMap<>();
+        //Cholesterol Map
+        SortedMap<String,Integer> drugCholesterolHigh = new TreeMap<>();
+        SortedMap<String,Integer> drugCholesterolNormal = new TreeMap<>();
+        //Na Map
+        SortedMap<String,Integer> drugNaHigh = new TreeMap<>();
+        SortedMap<String,Integer> drugNaNormal = new TreeMap<>();
+        //K Map
+        SortedMap<String,Integer> drugKHigh = new TreeMap<>();
+        SortedMap<String,Integer> drugKNormal = new TreeMap<>();
+
+
+        //Age List
+        ArrayList<Double> ageList = initializeAge(personnes);
+
+        ArrayList<Double> naList = initializeNa(personnes);
+        ArrayList<Double> kList = initializeK(personnes);
+
+        double ageAverage = listeAverage(ageList);
+        int age;
+        String drug;
+        String genre;
+        String bloodPressure;
+        String cholesterol;
+        double na;
+        double naAverage = listeAverage(naList);
+        double k;
+        double kAverage = listeAverage(kList);
+
+
+        //getting the number of occurences per Drug
+        for (Personne p : personnes) {
+
+            drug = p.getDrug();
+
+            if(!drug.equals("null")) {
+
+                putInMap(drugMap, drug);
+
+                age = p.getAge();
+
+                if (age > ageAverage)
+                    putInMap(AgeMapHigh, drug);
+                else if(age < ageAverage)
+                    putInMap(AgeMapLow, drug);
+
+                genre = p.getGenre();
+
+                if(genre.equals("M"))
+                    putInMap(drugGenreM, drug);
+                else
+                    putInMap(drugGenreF, drug);
+
+                bloodPressure = p.getBloodPressure();
+
+                if(bloodPressure.equals("HIGH"))
+                    putInMap(drugBloodPressureHigh, drug);
+
+                else if(bloodPressure.equals("NORMAL"))
+                    putInMap(drugBloodPressureNormal, drug);
+
+                else
+                    putInMap(drugBloodPressureLow, drug);
+
+                cholesterol = p.getCholesterol();
+
+                if(cholesterol.equals("HIGH"))
+                    putInMap(drugCholesterolHigh, drug);
+                else
+                    putInMap(drugCholesterolNormal, drug);
+
+                na = p.getNa();
+
+                if(na > naAverage)
+                    putInMap(drugNaHigh, drug);
+                else
+                    putInMap(drugNaNormal, drug);
+
+                k = p.getK();
+
+                if(k > kAverage)
+                    putInMap(drugKHigh, drug);
+                else
+                    putInMap(drugKNormal, drug);
+            }
+        }
+
+
+        System.out.println("drug number :" + drugMap);
+
+        //Treatement of the target personne
+        System.out.println("Average age :" + ageAverage);
+        System.out.println("drug age high :" + AgeMapHigh);
+        System.out.println("drug age low :" + AgeMapLow);
+
+        System.out.println("drug genre M :" + drugGenreM);
+        System.out.println("drug genre F :" + drugGenreF);
+
+        System.out.println("drug blood pressure high :" + drugBloodPressureHigh);
+        System.out.println("drug blood pressure normal :" + drugBloodPressureNormal);
+        System.out.println("drug blood pressure low :" + drugBloodPressureLow);
+
+        System.out.println("drug cholesterol high :" + drugCholesterolHigh);
+        System.out.println("drug cholesterol normal :" + drugCholesterolNormal);
+
+        System.out.println("drug na high :" + drugNaHigh);
+        System.out.println("drug na normal :" + drugNaNormal);
+
+        System.out.println("drug k high :" + drugKHigh);
+        System.out.println("drug k normal :" + drugKNormal);
+
+        return "Naive bayes";
+    }
+
 }
--- app/src/main/java/com/example/projetandroid/AlgorithmChoiceActivity.java
+++ app/src/main/java/com/example/projetandroid/AlgorithmChoiceActivity.java
@@ -5,9 +5,11 @@ import static com.example.projetandroid.Algorithm.mostFrequent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -22,34 +24,34 @@ public class AlgorithmChoiceActivity extends AppCompatActivity {
 
         DatabaseHandler db = new DatabaseHandler(this);
 
-        Button calculate = (Button) findViewById(R.id.calculate);
-        RadioGroup radioAlgo = (RadioGroup) findViewById(R.id.radio_Choice);
+        Button calculate =  findViewById(R.id.calculate);
+        RadioGroup radioAlgo =  findViewById(R.id.radio_Choice);
+
+        TextView recommendedDrug = findViewById(R.id.recommendedDrug);
+        TextView drugResult = findViewById(R.id.drugResult);
 
         calculate.setOnClickListener(view -> {
         try {
-            RadioButton selectedAlgo = (RadioButton) findViewById(radioAlgo.getCheckedRadioButtonId());
+            RadioButton selectedAlgo = findViewById(radioAlgo.getCheckedRadioButtonId());
             String selectedAlgoText = selectedAlgo.getText().toString();
 
             //getting the last added person in the database from their ROWID instead of sending the whole object from the previous activity
             Personne targetPersonne =  db.getLastAddedPersonne();
 
-            //helps having same name for the results of all the algorithms instead of having to change the name of the result for each algorithm
-            String drugResult = "";
+            String drugResultText = "";
 
             if(selectedAlgoText.equals("Nearest neighbor")){
 
                 TreeSet<Integer> nearestNeighbours;
 
-                //getting the nearest neighbours (an array of their ROWID since i use multiple rows as a primary key) of the last added personne
-                nearestNeighbours = Algorithm.NearestNeighbour(targetPersonne, db.getAllPersonnes());
+                //getting the drugs of the 5 nearest neighbours from the database
+                nearestNeighbours = Algorithm.NearestNeighbour(targetPersonne, db.getAllPersonnes() ,5);
 
                 Personne neighbour;
 
                 ArrayList<String> neighboursDrugs = new ArrayList<>();
 
-                //getting the drugs of the 5 nearest neighbours from the database
-                //can be changed to get the number of neighbours you want
-                //also can show all 5 drugs in a list instead of just the most frequent one
+
                 for (Integer rowid : nearestNeighbours) {
 
                    neighbour = db.getPersonneFromRowId(String.valueOf(rowid));
@@ -61,11 +63,20 @@ public class AlgorithmChoiceActivity extends AppCompatActivity {
                 targetPersonne.setDrug(mostFrequentDrug);
                 db.updatePersonne(targetPersonne);
 
-                drugResult = mostFrequentDrug;
+                drugResultText = mostFrequentDrug;
+            }else if(selectedAlgoText.equals("Naive Bayes")){
+
+                //getting the drug from the Naive Bayes algorithm
+               drugResultText = Algorithm.naiveBayes(targetPersonne, db.getAllPersonnes());
             }
 
+            drugResult.setText(drugResultText);
+            recommendedDrug.setVisibility(View.VISIBLE);
+            drugResult.setVisibility(View.VISIBLE);
+
+
             Toast.makeText(this,
-                    "results using " + selectedAlgoText  + " is " + drugResult,
+                    "results using " + selectedAlgoText  + " is " + drugResultText,
                     Toast.LENGTH_SHORT).show();
 
         }catch (NullPointerException e){
